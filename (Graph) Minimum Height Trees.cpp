4f45a9e7d2f936dc14e26ae1c60f61ff233c4b5f
GOPAL REDDY <gopalreddy465@gmail.com>
2024-02-17 20:46:19 +0530

    Update (Graph) Minimum Height Trees.cpp
    Added commented code to solve this without using struct GraphNode.

--- (Graph) Minimum Height Trees.cpp
+++ (Graph) Minimum Height Trees.cpp
@@ -72,4 +72,67 @@ public:
         set<GraphNode*> leafNodeList = getLeafNodesFromList(graph);
         return getFinalNodes(leafNodeList);
     }
+
+    // vector<int> getFinalNodes(vector<set<int>> &graph, set<int> &nodesToRemove){
+    //     set<int> newlyFormedLeafNodes;
+    //     for(auto IT=nodesToRemove.begin() ; IT!=nodesToRemove.end() ; IT++){
+    //         int currGraphNode = *IT;
+    //         if(graph[currGraphNode].size()>0) {
+    //             int connectedNode = *(graph[currGraphNode].begin());
+    //             if (nodesToRemove.find(connectedNode)==nodesToRemove.end()) {
+    //                 graph[currGraphNode].erase(connectedNode);
+    //                 graph[connectedNode].erase(currGraphNode);
+    //                 if (isGraphNodeALeafNode(graph[connectedNode])) {
+    //                     newlyFormedLeafNodes.insert(connectedNode);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if (newlyFormedLeafNodes.empty()){
+    //         vector<int> RES;
+    //         for(auto IT1=nodesToRemove.begin() ; IT1!=nodesToRemove.end() ; IT1++){
+    //             RES.push_back(*IT1);
+    //         }
+    //         return RES;
+    //     }
+    //     return getFinalNodes(graph, newlyFormedLeafNodes);
+    // }
+
+    // bool isGraphNodeHasLessThanOrEqualToOneConnection(set<int> connectedNodes){
+    //     return connectedNodes.size()<=1;
+    // }
+
+    // bool isGraphNodeALeafNode(set<int> connectedNodes){
+    //     return isGraphNodeHasLessThanOrEqualToOneConnection(connectedNodes);
+    // }
+
+    // void addGraphNodeToLeafNodeList(int graphNode, set<int> &leafNodes) {
+    //     leafNodes.insert(graphNode);
+    // }
+
+    // set<int> getLeafNodesFromList(vector<set<int>> &graph){
+    //     set<int> leafNodes;
+    //     for(int i=0 ; i<graph.size() ; i++){
+    //         if(isGraphNodeALeafNode(graph[i])){
+    //             addGraphNodeToLeafNodeList(i, leafNodes);
+    //         }
+    //     }
+    //     return leafNodes;
+    // }
+
+    // vector<set<int>> constructGraph(int n, vector<vector<int>>& edges){
+    //     vector<set<int>> graph(n, set<int>());
+    //     for(int i=0 ; i<edges.size() ; i++){
+    //         int x = edges[i][0], y = edges[i][1];
+    //         graph[x].insert(y);
+    //         graph[y].insert(x);
+    //     }
+    //     return graph;
+    // }
+
+    // vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+    //     vector<set<int>> graph = constructGraph(n, edges);
+    //     set<int> leafNodes = getLeafNodesFromList(graph);
+    //     return getFinalNodes(graph, leafNodes);
+    // }
 };
